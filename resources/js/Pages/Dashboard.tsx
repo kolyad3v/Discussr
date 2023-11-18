b5ee1b2dc8e8e4877e192236d21c6b6835caeafb
Nick <nickgillham@proton.me>
2023-11-18 10:21:35 +0000

    import header, sidebar begin, main begin

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,22 +1,30 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
-import { Head } from '@inertiajs/react'
+import { CssVarsProvider, Grid, Sheet } from '@mui/joy'
+
 import { PageProps } from '@/types'
+import Header from './Header'
 
 export default function Dashboard({ auth, conversationsData }: PageProps) {
 	console.log(conversationsData)
 	return (
-		<AuthenticatedLayout
-			user={auth.user}
-			header={<h2 className='font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight'>Dashboard</h2>}>
-			<Head title='Dashboard' />
+		<AuthenticatedLayout user={auth.user}>
+			<CssVarsProvider>
+				<Sheet>
+					<Grid
+						container
+						sx={{ minHeight: '100vh', height: '100%' }}>
+						<Header />
+						<h2> Sidebar</h2>
 
-			<div className='py-12'>
-				<div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
-					<div className='bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg'>
-						<div className='p-6 text-gray-900 dark:text-gray-100'>You're logged in!</div>
-					</div>
-				</div>
-			</div>
+						<Grid
+							xs
+							sx={{ zIndex: 0 }}>
+							<h2> Main</h2>
+							Sh
+						</Grid>
+					</Grid>
+				</Sheet>
+			</CssVarsProvider>
 		</AuthenticatedLayout>
 	)
 }
